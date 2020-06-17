b92f11de1f33d1c2d766c1df7a6b1dd01a908375
omidiora <omidioraemmanuel@gmail.com>
2020-06-17 19:34:24 +0100

    App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,132 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React, { Component } from 'react'
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+// class App extends React.Component {
+//   constructor(props){
+//     super(props);
+//     this.state = {
+//       items:[],
+//       currentItem:{
+//         text:'',
+//         key:''
+//       }
+//     }
+//     this.addItem = this.addItem.bind(this);
+//     this.handleInput = this.handleInput.bind(this);
+//   }
+//   addItem(e){
+//     e.preventDefault();
+//     const newItem = this.state.currentItem;
+//     if(newItem.text !==""){
+//       const items = [...this.state.items, newItem];
+//     this.setState({
+//       items: items,
+//       currentItem:{
+//         text:'',
+//         key:''
+//       }
+//     })
+//     }
+//   }
+//   handleInput(e){
+//     this.setState({
+//       currentItem:{
+//         text: e.target.value,
+//         key: Date.now()
+//       }
+//     })
+//   }
+//   deleteItem(key){
+//     const filteredItems= this.state.items.filter(item =>
+//       item.key!==key);
+//     this.setState({
+//       items: filteredItems
+//     })
+
+//   }
+   
+  
+//  render(){
+//   return (
+//     <div className="App">
+//       <header>
+//         <form id="to-do-form" onSubmit={this.addItem}>
+//           <input type="text" placeholder="Enter task" value= {this.state.currentItem.text} onChange={this.handleInput}></input>
+//           <button type="submit">Add</button>
+//         </form>
+//         <p>{this.state.items.text}</p>
+        
+        
+//       </header>
+//     </div>
+//   );
+//  }
+// }
+
+
+// export default App;
+
+
+import React from 'react'
+import './App.css'
+
+export class App extends React.Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      items:[],
+      currentItem:{
+        text:'',
+        key:''
+      }
+    }
+    this.handleInput=this.handleInput.bind(this);
+    this.addItem=this.addItem.bind(this);
+
+  }
+
+    handleInput(e){
+      this.setState({
+        currentItem:{
+        text:e.target.value,
+        key:Date.now(),
+        }
+      })
+    }
+
+    addItem(e){
+      e.preventDefault();
+      const newItem=this.state.currentItem;
+      console.log(newItem);
+      if(newItem.text!==''){
+        const newItems=[...this.state.items  , newItem];
+        this.setState({
+          items:newItems,
+          currentItem:{
+            text:'',
+            key:''
+          }
+        })
+
+      }
+    }
+  
+
+ 
+  render() {
+    return (
+      <header>
+        <form id="to-do-form" onSubmit={this.addItem}> 
+          <input type='text' placeholder='Enter Text'  value={this.state.currentItem.text} onChange={this.handleInput}></input>
+          <button type='button'>Add</button>
+        </form>
+        
       </header>
-    </div>
-  );
+    )
+  }
 }
 
-export default App;
+export default App
