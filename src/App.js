fa3f6989d4813386fbb412a1d8ac4bc790d0b975
omidiora <omidioraemmanuel@gmail.com>
2020-06-19 19:47:02 +0100

    listitem

--- src/App.js
+++ src/App.js
@@ -1,76 +1,12 @@
-// import React, { Component } from 'react'
-// import logo from './logo.svg';
-// import './App.css';
-
-
-// class App extends React.Component {
-//   constructor(props){
-//     super(props);
-//     this.state = {
-//       items:[],
-//       currentItem:{
-//         text:'',
-//         key:''
-//       }
-//     }
-//     this.addItem = this.addItem.bind(this);
-//     this.handleInput = this.handleInput.bind(this);
-//   }
-//   addItem(e){
-//     e.preventDefault();
-//     const newItem = this.state.currentItem;
-//     if(newItem.text !==""){
-//       const items = [...this.state.items, newItem];
-//     this.setState({
-//       items: items,
-//       currentItem:{
-//         text:'',
-//         key:''
-//       }
-//     })
-//     }
-//   }
-//   handleInput(e){
-//     this.setState({
-//       currentItem:{
-//         text: e.target.value,
-//         key: Date.now()
-//       }
-//     })
-//   }
-//   deleteItem(key){
-//     const filteredItems= this.state.items.filter(item =>
-//       item.key!==key);
-//     this.setState({
-//       items: filteredItems
-//     })
-
-//   }
-   
-  
-//  render(){
-//   return (
-//     <div className="App">
-//       <header>
-//         <form id="to-do-form" onSubmit={this.addItem}>
-//           <input type="text" placeholder="Enter task" value= {this.state.currentItem.text} onChange={this.handleInput}></input>
-//           <button type="submit">Add</button>
-//         </form>
-//         <p>{this.state.items.text}</p>
-        
-        
-//       </header>
-//     </div>
-//   );
-//  }
-// }
-
-
-// export default App;
 
 
 import React from 'react'
 import './App.css'
+import  ListItems from './ListItems'
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {faTrash} from '@fortawesome/free-solid-svg-icons';
+
+library.add(faTrash);
 
 export class App extends React.Component {
   constructor(props) {
@@ -123,6 +59,8 @@ export class App extends React.Component {
           <input type='text' placeholder='Enter Text'  value={this.state.currentItem.text} onChange={this.handleInput}></input>
           <button type='button'>Add</button>
         </form>
+
+      <ListItems items={this.state.items}/>
         
       </header>
     )
